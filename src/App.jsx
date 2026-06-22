4f5ba54d0379ff964a9e626c3c0cb427914a6876
Abhinand Krishna A <am.abhinandkrishna@gmail.com>
2026-06-22 13:18:25 +0530

    before add image option

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import TextEditor from './components/TextEditor';
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      {/* <h1 className="text-3xl font-bold underline">
     Editor JS
-  </h1>
+  </h1> */}
   <TextEditor />
     </div>
   );
